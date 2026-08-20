1bc407c6a812a4ce1734abcb5ddf1b4d446a5891
vasudev pareek <vasudevpareekbca@gmail.com>
2026-08-20 19:11:10 +0530

    Update extraction_of_digits.py

--- extraction_of_digits.py
+++ extraction_of_digits.py
@@ -1,8 +1,31 @@
-#count digit
+#count number
 n=12345
 num=n
 count=0
 while num>0:
     count+=1
     num=num//10
-print (count)
+print(count)
+
+#check Palindrome
+n=121
+num=n
+result=0
+while num>0:
+    ld=num%10
+    result=(result*10)+ld
+    num=num//10
+print( n==result)
+
+#armstrong number
+n=153
+num=n
+total=0
+nod=len(str(num))
+while num>0:
+    
+    ld=num%10
+    total+=ld**nod
+    num=num//10
+print(total==n)
+
