2262df2c7a0bbd736afac85bc3cb80aaace7861a
vasudev pareek <vasudevpareekbca@gmail.com>
2026-09-01 16:44:57 +0530

    Add files via upload

---
+++ Day11_with_dsa_insertion_sort.py
@@ -0,0 +1,18 @@
+def insertion_sort(arr):
+    
+    for i in range(1, len(arr)):
+        key = arr[i]  
+        j = i - 1
+        
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+            
+       
+        arr[j + 1] = key
+
+
+nums = [5, 2, 4, 6, 1, 3]
+insertion_sort(nums)
+print("Sorted Array:", nums)
